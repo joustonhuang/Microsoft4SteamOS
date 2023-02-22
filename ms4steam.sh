b599529c5f5986e34c6f7a0df757185afa608e58
joustonhuang <115980414+joustonhuang@users.noreply.github.com>
2023-02-22 20:33:23 +0800

    Update ms4steam.sh

--- ms4steam.sh
+++ ms4steam.sh
@@ -9,6 +9,8 @@ sudo pacman-key --populate archlinux
 
 
 InstallYay(){
+# install git
+sudo pacman -S base-devel git --noconfirm
 # Clone yay
 cd /opt
 #sudo git clone https://aur.archlinux.org/yay.git
